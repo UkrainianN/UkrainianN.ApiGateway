0f02d76ce42a5e09c8ff15faed756e664bf5f0cb
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-08-27 22:49:00 +0300

    Added UsersController

---
+++ UkrainianN.ApiGateway/UkrainianN.ApiGateway/Controllers/V1/UsersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace UkrainianN.ApiGateway.Controllers.V1
+{
+    public class UsersController : ApiBaseController
+    {
+        [HttpGet("{id}")]
+        public IActionResult GetUserById()
+        {
+            return Ok();
+        }
+    }
+}
