13e656d034f42ad6eb988a5e2d5180c7741e5937
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-08-27 22:38:24 +0300

    Added IdentityController

---
+++ UkrainianN.ApiGateway/UkrainianN.ApiGateway/Controllers/V1/IdentityController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UkrainianN.ApiGateway.Controllers.V1
+{
+    public class IdentityController : ApiBaseController
+    {
+        [HttpPost("register")]
+        public IActionResult Register()
+        {
+            return Ok();
+        }
+
+        [HttpPost("confirm")]
+        public IActionResult Confirm()
+        {
+            return Ok();
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login()
+        {
+            return Ok();
+        }
+
+        [HttpDelete("logout")]
+        public IActionResult Logout()
+        {
+            return Ok();
+        }
+
+        [HttpGet("sessions")]
+        public IActionResult GetSessions(byte option = 0)
+        {
+            return Ok();
+        }
+
+        [HttpGet("sessions/{id}")]
+        public IActionResult GetSessionById(long id)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("sessions")]
+        public IActionResult CloseSessions(byte option = 0)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("sessions/{id}")]
+        public IActionResult CloseSessionById(long id)
+        {
+            return Ok();
+        }
+    }
+}
